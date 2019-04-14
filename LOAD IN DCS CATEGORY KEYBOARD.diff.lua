389ef300e05cd11dc4f868717cf9f478a6eed4af
Sebastien LONGO <ext.slongo@cma-cgm.com>
2019-04-14 12:32:18 +0200

    add diff lua files + rename tmc file + create pptx and pdf mapping doc

---
+++ LOAD IN DCS CATEGORY KEYBOARD.diff.lua
@@ -0,0 +1,987 @@
+local diff = {
+	["keyDiffs"] = {
+		["d1533pnilunilcd7vd0vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Z",
+					["reformers"] = {
+						[1] = "RCtrl",
+						[2] = "RShift",
+					},
+				},
+			},
+			["name"] = "Ejection Seat SAFE/ARMED Handle - SAFE/ARMED",
+		},
+		["d177pnilunilcdnilvdnilvpnilvunil"] = {
+			["name"] = "Zoom normal",
+			["removed"] = {
+				[1] = {
+					["key"] = "NumEnter",
+				},
+			},
+		},
+		["d17pnilu18cdnilvdnilvpnilvunil"] = {
+			["name"] = "Cockpit panel view in",
+			["removed"] = {
+				[1] = {
+					["key"] = "Num0",
+				},
+			},
+		},
+		["d3001pnilu3001cd12vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "A",
+					["reformers"] = {
+						[1] = "LShift",
+					},
+				},
+			},
+			["name"] = "APU Control Switch - ON/OFF",
+		},
+		["d3001pnilu3001cd7vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "I",
+					["reformers"] = {
+						[1] = "LCtrl",
+						[2] = "LShift",
+					},
+				},
+			},
+			["name"] = "Canopy Control Switch - OPEN",
+		},
+		["d3001pnilunilcd10vd0vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "O",
+					["reformers"] = {
+						[1] = "LCtrl",
+					},
+				},
+			},
+			["name"] = "OBOGS Control Switch - OFF",
+		},
+		["d3001pnilunilcd10vd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "O",
+					["reformers"] = {
+						[1] = "LShift",
+					},
+				},
+			},
+			["name"] = "OBOGS Control Switch - ON",
+		},
+		["d3001pnilunilcd35vd0.2vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "[",
+					["reformers"] = {
+						[1] = "RShift",
+					},
+				},
+			},
+			["name"] = "Left MDI Brightness Selector Knob - DAY",
+		},
+		["d3001pnilunilcd35vd0vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "[",
+					["reformers"] = {
+						[1] = "RCtrl",
+					},
+				},
+			},
+			["name"] = "Left MDI Brightness Selector Knob - OFF",
+		},
+		["d3001pnilunilcd36vd0.2vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "\\",
+					["reformers"] = {
+						[1] = "RShift",
+					},
+				},
+			},
+			["name"] = "Right MDI Brightness Selector Knob - DAY",
+		},
+		["d3001pnilunilcd36vd0vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "\\",
+					["reformers"] = {
+						[1] = "RCtrl",
+					},
+				},
+			},
+			["name"] = "Right MDI Brightness Selector Knob - OFF",
+		},
+		["d3001pnilunilcd3vd0vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "B",
+					["reformers"] = {
+						[1] = "RCtrl",
+					},
+				},
+			},
+			["name"] = "Battery Switch - OFF",
+		},
+		["d3001pnilunilcd3vd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "B",
+					["reformers"] = {
+						[1] = "RShift",
+					},
+				},
+			},
+			["name"] = "Battery Switch - ON",
+		},
+		["d3001pnilunilcd54vd0.1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "E",
+					["reformers"] = {
+						[1] = "LAlt",
+						[2] = "LCtrl",
+					},
+				},
+			},
+			["name"] = "DISPENSER Switch - ON",
+		},
+		["d3001pnilunilcd54vd0vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "E",
+					["reformers"] = {
+						[1] = "LCtrl",
+						[2] = "LShift",
+					},
+				},
+			},
+			["name"] = "DISPENSER Switch - OFF",
+		},
+		["d3002pnilu3002cd7vd-1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "I",
+					["reformers"] = {
+						[1] = "LCtrl",
+					},
+				},
+			},
+			["name"] = "Canopy Control Switch - CLOSE",
+		},
+		["d3002pnilunilcd6vd0vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "X",
+					["reformers"] = {
+						[1] = "LAlt",
+						[2] = "LShift",
+					},
+				},
+			},
+			["name"] = "Probe Control Switch - RETRACT",
+		},
+		["d3002pnilunilcd6vd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "X",
+					["reformers"] = {
+						[1] = "LCtrl",
+						[2] = "LShift",
+					},
+				},
+			},
+			["name"] = "Probe Control Switch - EXTEND",
+		},
+		["d3002pnilunilcd7vd0vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "I",
+					["reformers"] = {
+						[1] = "LShift",
+					},
+				},
+			},
+			["name"] = "Canopy Control Switch - HOLD",
+		},
+		["d3003pnilu3003cd2vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = ";",
+					["reformers"] = {
+						[1] = "LCtrl",
+					},
+				},
+			},
+			["name"] = "FCS RESET Button",
+		},
+		["d3003pnilunilcd10vd0vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "O",
+					["reformers"] = {
+						[1] = "LCtrl",
+						[2] = "LShift",
+					},
+				},
+			},
+			["name"] = "OBOGS Control Switch - ON/OFF",
+		},
+		["d3003pnilunilcd42vd-1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "K",
+					["reformers"] = {
+						[1] = "LCtrl",
+					},
+				},
+			},
+			["name"] = "RADAR Switch - CCW",
+		},
+		["d3003pnilunilcd42vd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "K",
+					["reformers"] = {
+						[1] = "LShift",
+					},
+				},
+			},
+			["name"] = "RADAR Switch - CW",
+		},
+		["d3003pnilunilcd8vd-1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "C",
+					["reformers"] = {
+						[1] = "RCtrl",
+					},
+				},
+			},
+			["name"] = "STROBE Lights Switch - DIM",
+		},
+		["d3003pnilunilcd8vd0vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "C",
+					["reformers"] = {
+						[1] = "RCtrl",
+						[2] = "RShift",
+					},
+				},
+			},
+			["name"] = "STROBE Lights Switch - OFF",
+		},
+		["d3003pnilunilcd8vd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "C",
+					["reformers"] = {
+						[1] = "RShift",
+					},
+				},
+			},
+			["name"] = "STROBE Lights Switch - BRT",
+		},
+		["d3004pnilu3004cd23vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "A",
+					["reformers"] = {
+						[1] = "LCtrl",
+						[2] = "LShift",
+					},
+				},
+			},
+			["name"] = "Emergency Jettison Button",
+		},
+		["d3004pnilunilcd8vd0vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "L",
+					["reformers"] = {
+						[1] = "RAlt",
+					},
+				},
+			},
+			["name"] = "LDG/TAXI LIGHT Switch - OFF",
+		},
+		["d3004pnilunilcd8vd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "L",
+					["reformers"] = {
+						[1] = "RAlt",
+						[2] = "RShift",
+					},
+				},
+			},
+			["name"] = "LDG/TAXI LIGHT Switch - ON",
+		},
+		["d3006pnilu3006cd12vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "F",
+					["reformers"] = {
+						[1] = "RShift",
+					},
+				},
+			},
+			["name"] = "Fire and Bleed Air Test Switch - TEST A",
+		},
+		["d3006pnilunilcd7vd0vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Z",
+					["reformers"] = {
+						[1] = "RShift",
+					},
+				},
+			},
+			["name"] = "Ejection Seat SAFE/ARMED Handle - ARMED",
+		},
+		["d3006pnilunilcd7vd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Z",
+					["reformers"] = {
+						[1] = "RCtrl",
+					},
+				},
+			},
+			["name"] = "Ejection Seat SAFE/ARMED Handle - SAFE",
+		},
+		["d3007pnilu3007cd12vd-1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "F",
+					["reformers"] = {
+						[1] = "RCtrl",
+					},
+				},
+			},
+			["name"] = "Fire and Bleed Air Test Switch - TEST B",
+		},
+		["d3008pnilu3008cd5vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "J",
+					["reformers"] = {
+						[1] = "LCtrl",
+					},
+				},
+			},
+			["name"] = "Launch Bar Control Switch - EXTEND/RETRACT",
+		},
+		["d3009pnilunilcd5vd0vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "H",
+					["reformers"] = {
+						[1] = "LCtrl",
+					},
+				},
+			},
+			["name"] = "Arresting Hook Handle - Down",
+		},
+		["d3009pnilunilcd5vd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "H",
+					["reformers"] = {
+						[1] = "LShift",
+					},
+				},
+			},
+			["name"] = "Arresting Hook Handle - Up",
+		},
+		["d3009pnilunilcd7vd0vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "X",
+					["reformers"] = {
+						[1] = "RCtrl",
+					},
+				},
+			},
+			["name"] = "Shoulder Harness Control Handle - UNLOCK",
+		},
+		["d3009pnilunilcd7vd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "X",
+					["reformers"] = {
+						[1] = "RShift",
+					},
+				},
+			},
+			["name"] = "Shoulder Harness Control Handle - LOCK",
+		},
+		["d3010pnilu3010cd23vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "A",
+					["reformers"] = {
+						[1] = "LAlt",
+						[2] = "LCtrl",
+					},
+				},
+			},
+			["name"] = "Selective Jettison Pushbutton",
+		},
+		["d3011pnilunilcd23vd0.2vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "S",
+					["reformers"] = {
+						[1] = "LAlt",
+						[2] = "LCtrl",
+					},
+				},
+			},
+			["name"] = "Selective Jettison Knob - RACK/LCHR",
+		},
+		["d3011pnilunilcd23vd0vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "S",
+					["reformers"] = {
+						[1] = "LCtrl",
+						[2] = "LShift",
+					},
+				},
+			},
+			["name"] = "Selective Jettison Knob - SAFE",
+		},
+		["d3014pnilunilcd53vd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "R",
+					["reformers"] = {
+						[1] = "RAlt",
+					},
+				},
+			},
+			["name"] = "ALR-67 POWER Pushbutton - ON/OFF",
+		},
+		["d3015pnilu3015cd12vd-1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = ",",
+					["reformers"] = {
+						[1] = "RShift",
+					},
+				},
+			},
+			["name"] = "Engine Crank Switch - LEFT",
+		},
+		["d3016pnilu3016cd12vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "/",
+					["reformers"] = {
+						[1] = "RShift",
+					},
+				},
+			},
+			["name"] = "Engine Crank Switch - RIGHT",
+		},
+		["d3016pnilunilcd12vd-1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = ".",
+					["reformers"] = {
+						[1] = "RShift",
+					},
+				},
+			},
+			["name"] = "Engine Crank Switch - OFF",
+		},
+		["d3018pnilu3018cd25vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Num0",
+				},
+			},
+			["name"] = "UFC Keyboard Pushbutton - 0",
+		},
+		["d3019pnilu3019cd25vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Num1",
+				},
+			},
+			["name"] = "UFC Keyboard Pushbutton - 1",
+		},
+		["d3020pnilu3020cd25vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Num2",
+				},
+			},
+			["name"] = "UFC Keyboard Pushbutton - 2",
+		},
+		["d3020pnilunilcd9vd-0.1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Insert",
+					["reformers"] = {
+						[1] = "LCtrl",
+					},
+				},
+			},
+			["name"] = "Flashlight BRT Control - Decrease",
+		},
+		["d3020pnilunilcd9vd0.1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Insert",
+					["reformers"] = {
+						[1] = "LShift",
+					},
+				},
+			},
+			["name"] = "Flashlight BRT Control - Increase",
+		},
+		["d3021pnilu3021cd25vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Num3",
+				},
+			},
+			["name"] = "UFC Keyboard Pushbutton - 3",
+		},
+		["d3022pnilu3022cd25vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Num4",
+				},
+			},
+			["name"] = "UFC Keyboard Pushbutton - 4",
+		},
+		["d3023pnilu3023cd25vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Num5",
+				},
+			},
+			["name"] = "UFC Keyboard Pushbutton - 5",
+		},
+		["d3024pnilu3024cd25vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Num6",
+				},
+			},
+			["name"] = "UFC Keyboard Pushbutton - 6",
+		},
+		["d3024pnilunilcd2vd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "P",
+				},
+			},
+			["name"] = "Cycle Afterburner Detent - ON/OFF",
+		},
+		["d3025pnilu3025cd25vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Num7",
+				},
+			},
+			["name"] = "UFC Keyboard Pushbutton - 7",
+		},
+		["d3026pnilu3026cd25vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Num8",
+				},
+			},
+			["name"] = "UFC Keyboard Pushbutton - 8",
+		},
+		["d3027pnilu3027cd25vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Num9",
+				},
+			},
+			["name"] = "UFC Keyboard Pushbutton - 9",
+		},
+		["d3029pnilu3029cd25vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "NumEnter",
+				},
+			},
+			["name"] = "UFC Keyboard Pushbutton - ENT",
+		},
+		["d3040pnilu3040cd13vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "9",
+					["reformers"] = {
+						[1] = "LAlt",
+					},
+				},
+			},
+			["name"] = "Throttle Finger Lift (Both) - UP/DOWN",
+			["removed"] = {
+				[1] = {
+					["key"] = "0",
+				},
+			},
+		},
+		["d3041pnilunilcd13vd0vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "T",
+					["reformers"] = {
+						[1] = "LShift",
+					},
+				},
+			},
+			["name"] = "Exterior Lights Switch - OFF",
+		},
+		["d3041pnilunilcd13vd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "T",
+					["reformers"] = {
+						[1] = "LCtrl",
+					},
+				},
+			},
+			["name"] = "Exterior Lights Switch - ON",
+		},
+		["d3045pnilu3045cd13vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "OEM102",
+				},
+			},
+			["name"] = "Sensor Control Switch - Depress",
+		},
+		["dnilp210u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Up Right slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "Num9",
+				},
+			},
+		},
+		["dnilp211u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Down Right slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "Num3",
+				},
+			},
+		},
+		["dnilp212u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Down Left slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "Num1",
+				},
+			},
+		},
+		["dnilp213u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Up Left slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "Num7",
+				},
+			},
+		},
+		["dnilp3002unilcd58vdnilvp-1vunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "J",
+					["reformers"] = {
+						[1] = "LAlt",
+						[2] = "RShift",
+					},
+				},
+			},
+			["name"] = "HMD OFF/BRT Knob - CCW/Decrease",
+		},
+		["dnilp3002unilcd58vdnilvp1vunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "J",
+					["reformers"] = {
+						[1] = "LAlt",
+						[2] = "RCtrl",
+					},
+				},
+			},
+			["name"] = "HMD OFF/BRT Knob - CW/Increase",
+		},
+		["dnilp3005unilcd8vdnilvp-1vunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "V",
+					["reformers"] = {
+						[1] = "LCtrl",
+					},
+				},
+			},
+			["name"] = "POSITION Lights Dimmer Control - CCW/Left",
+		},
+		["dnilp3005unilcd8vdnilvp1vunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "V",
+					["reformers"] = {
+						[1] = "LShift",
+					},
+				},
+			},
+			["name"] = "POSITION Lights Dimmer Control - CW/Right",
+		},
+		["dnilp3007unilcd8vdnilvp-1vunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "V",
+					["reformers"] = {
+						[1] = "RCtrl",
+					},
+				},
+			},
+			["name"] = "FORMATION Lights Dimmer Control - CCW/Left",
+		},
+		["dnilp3007unilcd8vdnilvp1vunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "V",
+					["reformers"] = {
+						[1] = "RShift",
+					},
+				},
+			},
+			["name"] = "FORMATION Lights Dimmer Control - CW/Right",
+		},
+		["dnilp3010unilcd9vdnilvp-0.5vunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Q",
+					["reformers"] = {
+						[1] = "RCtrl",
+					},
+				},
+			},
+			["name"] = "CONSOLES Lights Dimmer Control - CCW/Decrease",
+		},
+		["dnilp3010unilcd9vdnilvp0.5vunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Q",
+					["reformers"] = {
+						[1] = "RShift",
+					},
+				},
+			},
+			["name"] = "CONSOLES Lights Dimmer Control - CW/Increase",
+		},
+		["dnilp3011unilcd34vdnilvp-0.5vunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "E",
+					["reformers"] = {
+						[1] = "RCtrl",
+					},
+				},
+			},
+			["name"] = "HUD Symbology Brightness Control Knob - CCW/Decrease",
+		},
+		["dnilp3011unilcd34vdnilvp0.5vunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "E",
+					["reformers"] = {
+						[1] = "RShift",
+					},
+				},
+			},
+			["name"] = "HUD Symbology Brightness Control Knob - CW/Increase",
+		},
+		["dnilp3012unilcd9vdnilvp-0.5vunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "W",
+					["reformers"] = {
+						[1] = "RCtrl",
+					},
+				},
+			},
+			["name"] = "INST PNL Dimmer Control - CCW/Decrease",
+		},
+		["dnilp3012unilcd9vdnilvp0.5vunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "W",
+					["reformers"] = {
+						[1] = "RShift",
+					},
+				},
+			},
+			["name"] = "INST PNL Dimmer Control - CW/Increase",
+		},
+		["dnilp3014unilcd9vdnilvp-0.5vunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "U",
+					["reformers"] = {
+						[1] = "RCtrl",
+					},
+				},
+			},
+			["name"] = "FLOOD Light Dimmer Control - CCW/Decrease",
+		},
+		["dnilp3014unilcd9vdnilvp0.5vunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "U",
+					["reformers"] = {
+						[1] = "RShift",
+					},
+				},
+			},
+			["name"] = "FLOOD Light Dimmer Control - CW/Increase",
+		},
+		["dnilp3017unilcd9vdnilvp-0.5vunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "A",
+					["reformers"] = {
+						[1] = "RCtrl",
+					},
+				},
+			},
+			["name"] = "CHART Light Dimmer Control - CCW/Decrease",
+		},
+		["dnilp3017unilcd9vdnilvp0.5vunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "A",
+					["reformers"] = {
+						[1] = "RShift",
+					},
+				},
+			},
+			["name"] = "CHART Light Dimmer Control - CW/Increase",
+		},
+		["dnilp3041unilcd37vdnilvp-0.5vunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "'",
+					["reformers"] = {
+						[1] = "RCtrl",
+					},
+				},
+			},
+			["name"] = "AMPCD Off/Brightness Control Knob - CCW/Decrease",
+		},
+		["dnilp3041unilcd37vdnilvp0.5vunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "'",
+					["reformers"] = {
+						[1] = "RShift",
+					},
+				},
+			},
+			["name"] = "AMPCD Off/Brightness Control Knob - CW/Increase",
+		},
+		["dnilp3042unilcd35vdnilvp-0.5vunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = ";",
+					["reformers"] = {
+						[1] = "LAlt",
+					},
+				},
+			},
+			["name"] = "Left MDI Brightness Control Knob - CCW/Decrease",
+		},
+		["dnilp3042unilcd35vdnilvp0.5vunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = ";",
+					["reformers"] = {
+						[1] = "LShift",
+					},
+				},
+			},
+			["name"] = "Left MDI Brightness Control Knob - CW/Increase",
+		},
+		["dnilp3042unilcd36vdnilvp-0.5vunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "]",
+					["reformers"] = {
+						[1] = "RCtrl",
+					},
+				},
+			},
+			["name"] = "Right MDI Brightness Control Knob - CCW/Decrease",
+		},
+		["dnilp3042unilcd36vdnilvp0.5vunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "]",
+					["reformers"] = {
+						[1] = "RShift",
+					},
+				},
+			},
+			["name"] = "Right MDI Brightness Control Knob - CW/Increase",
+		},
+		["dnilp32u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Left slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "Num4",
+				},
+			},
+		},
+		["dnilp33u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Right slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "Num6",
+				},
+			},
+		},
+		["dnilp34u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Up slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "Num8",
+				},
+			},
+		},
+		["dnilp35u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Down slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "Num2",
+				},
+			},
+		},
+		["dnilp36unilcdnilvdnilvpnilvunil"] = {
+			["name"] = "View Center",
+			["removed"] = {
+				[1] = {
+					["key"] = "Num5",
+				},
+			},
+		},
+	},
+}
+return diff
---
+++ LOAD IN DCS CATEGORY combined.diff.lua
@@ -0,0 +1,211 @@
+local diff = {
+	["axisDiffs"] = {
+		["a2002cdnil"] = {
+			["changed"] = {
+				[1] = {
+					["filter"] = {
+						["curvature"] = {
+							[1] = 0.05,
+						},
+						["deadzone"] = 0.03,
+						["invert"] = false,
+						["saturationX"] = 1,
+						["saturationY"] = 1,
+						["slider"] = false,
+					},
+					["key"] = "JOY_X",
+				},
+			},
+			["name"] = "Roll",
+		},
+		["a2003cdnil"] = {
+			["name"] = "Rudder",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_RZ",
+				},
+			},
+		},
+		["a2004cdnil"] = {
+			["name"] = "Thrust",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_Z",
+				},
+			},
+		},
+		["a2005cdnil"] = {
+			["added"] = {
+				[1] = {
+					["filter"] = {
+						["curvature"] = {
+							[1] = 0.01,
+							[2] = 0.15,
+							[3] = 0.28,
+							[4] = 0.39,
+							[5] = 0.51,
+							[6] = 0.62,
+							[7] = 0.71,
+							[8] = 0.8,
+							[9] = 0.87,
+							[10] = 0.94,
+							[11] = 1,
+						},
+						["deadzone"] = 0,
+						["invert"] = false,
+						["saturationX"] = 1,
+						["saturationY"] = 1,
+						["slider"] = true,
+					},
+					["key"] = "JOY_RZ",
+				},
+			},
+			["name"] = "Thrust Left",
+		},
+		["a2006cdnil"] = {
+			["added"] = {
+				[1] = {
+					["filter"] = {
+						["curvature"] = {
+							[1] = 0.01,
+							[2] = 0.15,
+							[3] = 0.28,
+							[4] = 0.39,
+							[5] = 0.51,
+							[6] = 0.62,
+							[7] = 0.71,
+							[8] = 0.8,
+							[9] = 0.87,
+							[10] = 0.94,
+							[11] = 1,
+						},
+						["deadzone"] = 0,
+						["invert"] = false,
+						["saturationX"] = 1,
+						["saturationY"] = 1,
+						["slider"] = true,
+					},
+					["key"] = "JOY_Z",
+				},
+			},
+			["name"] = "Thrust Right",
+		},
+		["a2012cdnil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_RY",
+				},
+			},
+			["name"] = "Zoom View",
+		},
+		["a3026cd13"] = {
+			["added"] = {
+				[1] = {
+					["filter"] = {
+						["curvature"] = {
+							[1] = 0,
+						},
+						["deadzone"] = 0,
+						["invert"] = true,
+						["saturationX"] = 1,
+						["saturationY"] = 1,
+						["slider"] = false,
+					},
+					["key"] = "JOY_SLIDER2",
+				},
+			},
+			["name"] = "Radar Elevation Control",
+		},
+	},
+	["keyDiffs"] = {
+		["d179pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN2",
+				},
+			},
+			["name"] = "Communication menu",
+		},
+		["d3002pnilu3002cd6vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN17",
+				},
+			},
+			["name"] = "Probe Control Switch - EXTEND/RETRACT",
+		},
+		["d3023pnilu3023cd12vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN20",
+				},
+			},
+			["name"] = "APU Control Sw (special) - ON/OFF",
+		},
+		["dnilp210u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Up Right slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_UR",
+				},
+			},
+		},
+		["dnilp211u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Down Right slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_DR",
+				},
+			},
+		},
+		["dnilp212u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Down Left slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_DL",
+				},
+			},
+		},
+		["dnilp213u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Up Left slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_UL",
+				},
+			},
+		},
+		["dnilp32u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Left slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_L",
+				},
+			},
+		},
+		["dnilp33u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Right slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_R",
+				},
+			},
+		},
+		["dnilp34u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Up slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_U",
+				},
+			},
+		},
+		["dnilp35u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Down slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_D",
+				},
+			},
+		},
+	},
+}
+return diff
